a8021b49d86eb7e31c3e9a08fbecefadf8b339b9
SourabhPiramal <sourabh@piramalswasthya.org>
2024-11-11 12:26:57 +0530

    Clean solution files

--- src/main/java/com/piramal/spring_boot_demo/repository/FormRepository.java
+++
@@ -1,9 +0,0 @@
-package com.piramal.spring_boot_demo.repository;
-
-import com.piramal.spring_boot_demo.models.Form;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FormRepository extends JpaRepository<Form, Long> {
-}
