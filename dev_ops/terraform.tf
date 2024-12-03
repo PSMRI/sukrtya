47ec9d0f6da7f670a2c55812ce4c648ad95358a1
PiramalManish <manishkumar.singh1@piramalswasthya.org>
2024-12-04 02:59:51 +0530

    nginix configuration

--- dev_ops/terraform.tf
+++ dev_ops/terraform.tf
@@ -28,7 +28,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.47.0"
     }
-
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.8.0"
+    }
     random = {
       source  = "hashicorp/random"
       version = "~> 3.6.1"
