ad1d27e77eca31cb10575837d8b676daf5babd4e
SourabhPiramal <146918029+SourabhPiramal@users.noreply.github.com>
2025-02-28 15:37:19 +0530

    Revert "TEC-65"

--- src/main/java/com/piramal/sukrtya/services/ChangePasswordService.java
+++ src/main/java/com/piramal/sukrtya/services/ChangePasswordService.java
@@ -77,17 +77,4 @@ public class ChangePasswordService {
 
         return false;
     }
-
-
-
-    public ApiResponse<?> getProfile(String authHeader) {
-        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
-            throw new UnauthorizedException("Authorization token is missing or malformed");
-        }
-        String jwtToken = authHeader.substring(7); // Extract the token part by removing "Bearer "
-        String username = jwtUtil.extractUsername(jwtToken);
-
-        return new ApiResponse<>("success", username, null);
-    }
-
 }
