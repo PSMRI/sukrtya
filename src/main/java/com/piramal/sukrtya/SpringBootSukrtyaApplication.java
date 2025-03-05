3d782819d91ee39d40181ec41f8cde360da6b1e7
chandan_piramal <chandankumar@piramalswasthya.org>
2025-03-05 14:52:57 +0530

    server dropdoown change

--- src/main/java/com/piramal/sukrtya/SpringBootSukrtyaApplication.java
+++ src/main/java/com/piramal/sukrtya/SpringBootSukrtyaApplication.java
@@ -1,9 +1,14 @@
 package com.piramal.sukrtya;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@OpenAPIDefinition(servers = {
+		@Server(url = "https://api.sukrtya.in", description = "Production Server")
+})
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "com.piramal.sukrtya.repository", repositoryFactoryBeanClass = org.springframework.data.jpa.repository.support.JpaRepositoryFactoryBean.class)
 
