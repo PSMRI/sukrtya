a9474dadee8d4db79a4cbed89e5b8eb820f53330
PiramalManish <141394810+PiramalManish@users.noreply.github.com>
2025-03-07 00:57:21 +0530

    Update WebConfig.java
    remove traling / like https://ui.sukrtya.in/

--- src/main/java/com/piramal/sukrtya/config/WebConfig.java
+++ src/main/java/com/piramal/sukrtya/config/WebConfig.java
@@ -17,7 +17,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**") // Apply to all endpoints
-                        .allowedOrigins("https://ui.sukrtya.in/","https://api.sukrtya.in/","http://localhost:3000","https://localhost:3000") // Replace with your frontend URL
+                        .allowedOrigins("https://ui.sukrtya.in","https://api.sukrtya.in","http://localhost:3000","https://localhost:3000") // Replace with your frontend URL
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allowed HTTP methods                        
                         .allowCredentials(true); // Allow credentials (e.g., cookies)
             }
