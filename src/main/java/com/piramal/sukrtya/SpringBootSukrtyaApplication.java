4a842b612c6e98484248feecbecddfa3d73f2561
chandan_piramal <chandankumar@piramalswasthya.org>
2025-03-29 16:33:09 +0530

    host.docker.internal

--- src/main/java/com/piramal/sukrtya/SpringBootSukrtyaApplication.java
+++ src/main/java/com/piramal/sukrtya/SpringBootSukrtyaApplication.java
@@ -7,10 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @OpenAPIDefinition(servers = {
-		@Server(url = "https://api.sukrtya.in", description = "Production Server"),
 		@Server(url = "https://sukrtya.api.shsbihar.in", description = "104 - Production Server"),
+		@Server(url = "https://api.sukrtya.in", description = "Production Server"),
 		@Server(url = "https://dev-ui.sukrtya.in", description = "Tekdi - Production Server"),
-		@Server(url = "http://localhost:8081", description = "Development Server")
+		@Server(url = "http://localhost:8080", description = "Development Server")
 })
 
 @SpringBootApplication
