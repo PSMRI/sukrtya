6a0766488a9a3e05f1e473db7c5f08671d1d72d8
chandan_piramal <chandankumar@piramalswasthya.org>
2025-03-30 18:00:16 +0530

    jaeger

--- src/main/java/com/piramal/sukrtya/config/OtlpConfiguration.java
+++ src/main/java/com/piramal/sukrtya/config/OtlpConfiguration.java
@@ -1,6 +1,10 @@
 package com.piramal.sukrtya.config;
 
+import io.opentelemetry.api.OpenTelemetry;
 import io.opentelemetry.exporter.otlp.http.trace.OtlpHttpSpanExporter;
+import io.opentelemetry.sdk.OpenTelemetrySdk;
+import io.opentelemetry.sdk.trace.SdkTracerProvider;
+import io.opentelemetry.sdk.trace.export.SimpleSpanProcessor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,5 +17,18 @@ public class OtlpConfiguration {
         return OtlpHttpSpanExporter.builder()
             .setEndpoint(url)
             .build();
+    }
+    @Bean
+    public SdkTracerProvider sdkTracerProvider(OtlpHttpSpanExporter otlpHttpSpanExporter) {
+        return SdkTracerProvider.builder()
+                .addSpanProcessor(SimpleSpanProcessor.create(otlpHttpSpanExporter))
+                .build();
+    }
+
+    @Bean
+    public OpenTelemetry openTelemetry(SdkTracerProvider sdkTracerProvider) {
+        return OpenTelemetrySdk.builder()
+                .setTracerProvider(sdkTracerProvider)
+                .build();
     }
 }
