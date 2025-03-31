33ba884a4cadb5ff7af56b572f5c3f5aa609d701
chandan_piramal <chandankumar@piramalswasthya.org>
2025-03-31 16:30:25 +0530

    Current Server

--- src/main/java/com/piramal/sukrtya/config/OpenApiConfig.java
+++ src/main/java/com/piramal/sukrtya/config/OpenApiConfig.java
@@ -5,21 +5,21 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenApiConfig {
 
-    @Autowired
+    @Value("${server.url:http://localhost:8080}")
     private String serverUrl;
 
     @Bean
     public OpenAPI customOpenAPI() {
         final String securitySchemeName = "bearerAuth";
         return new OpenAPI()
-                .addServerItem(new Server().url(serverUrl).description("Current Server"))
+                .addServersItem(new Server().url(serverUrl).description("Current Server"))
                 .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
                 .components(new Components()
                         .addSecuritySchemes(securitySchemeName,
