c143821de039916d8462c99bde3b1d1421f7837c
Chandan Kumar <chandankumar@piramalswasthya.org>
2025-06-23 17:57:22 +0530

    add dev domains

--- src/main/java/com/piramal/sukrtya/config/WebConfig.java
+++ src/main/java/com/piramal/sukrtya/config/WebConfig.java
@@ -20,7 +20,7 @@ public class WebConfig {
                         .allowedOrigins(
                                 "https://sukrtya.api.shsbihar.in",
                                 "https://sukrtya.ui.shsbihar.in",
-                                "https://ui.sukrtya.in","https://api.sukrtya.in","https://dev-api.sukrtya.in"
+                                "https://ui.sukrtya.in","https://api.sukrtya.in","https://dev-api.sukrtya.in","https://dev-api-sukrtya.tech4gov.info","https://dev-sukrtya.tech4gov.info"
                                 ,"http://localhost:3000","https://localhost:3000","https://dev-ui.sukrtya.in") // Replace with your frontend URL
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allowed HTTP methods
                         .allowedHeaders("*") // Allow specific headers
