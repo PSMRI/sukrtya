0f6dcd172db9d4f1107905f3050099a1c2c0e208
SourabhPiramal <sourabh@piramalswasthya.org>
2026-05-13 15:48:42 +0530

    Initial setup for Sukrtya Siwan Spring Boot project

---
+++ src/main/java/com/sukrtya/siwan/SukrtyaSiwanApplication.java
@@ -0,0 +1,13 @@
+package com.sukrtya.siwan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SukrtyaSiwanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SukrtyaSiwanApplication.class, args);
+	}
+
+}
